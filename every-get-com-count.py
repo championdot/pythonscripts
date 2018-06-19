9fc65311f26f0ae319f368ec01dd8262bd7cf9ba
xiejieling@gmail.com <xiejieling@gmail.com>
2018-06-19 11:35:41 +0800

    update every get com count

--- every-get-com-count.py
+++ every-get-com-count.py
@@ -1,32 +1,35 @@
 #!/usr/bin/env python
 import sys
-import chardet
-import pandas
 import requests
 from bs4 import BeautifulSoup
-
-'''every-get-com-count.py 
-   from URL Get com and net TLD SRS and DNS data set
-   '''
+import datetime 
 
 def GetHtmlTab(url):
+    
     html = requests.get(url)
     page = html.content.decode("utf-8")
     bsObj = BeautifulSoup(page, 'lxml')
     srsnav = bsObj.find('div',{"class": "col_6 shift_2 margin_bottom zone-counts stack_mobile"})
     dnsnav = bsObj.find('div', {"class": "col_10 shift_1 clearfix"})
     
-    for m in srsnav:
-       print m
-       print 'all srs data set'
-    
-    for l in dnsnav:
-        if len(l) <>0 :
-            continue;
-        print 'out dns data set'
-        print l
+    today = datetime.date.today()
+    yesterday = today - datetime.timedelta(days=1)
+    yed = yesterday.strftime("%Y%m%d")
 
+    with open (yed,'aw+') as f:
+        f.write('all srs data set\n')
+        for m in srsnav:
+            f.write(str(m))
+
+        f.write('all dns data set\n')
+        for l in dnsnav:
+            if len(l) <>0 :
+                continue;
+            f.write(str(l))
 
 if __name__ == '__main__':
+    '''every-get-com-count.py 
+    from URL Get com and net TLD SRS and DNS data set
+    '''
     url = 'https://www.verisign.com/en_US/channel-resources/domain-registry-products/zone-file/index.xhtml'
     GetHtmlTab(url)
